3313cf1ad7129afd8cd86292c463671130c0a2e5
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-11-10 18:43:20 +0100

    added toggle attribute

--- responsive.js
+++ responsive.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", main);
-var currentWidth = window.innerWidth;
-var currentHeight = window.innerHeight;
+window.addEventListener("load", toggleExecution);
+window.addEventListener("load", toggleHandler);
 window.addEventListener("resize", main);
 
-window.addEventListener("devicemotion", handleOrientation, true);
+
+
+var currentWidth = window.innerWidth;
+var currentHeight = window.innerHeight;
+
+
 
 const usable = ["disappear","resize"];
 
@@ -33,22 +38,21 @@ function main() {
     /*disappear*/
     var disappearElements = $('[disappear]')
     if (disappearElements.length > 0) {
-        console.log("There's hidable elements: disappearElements.length > 0");       
+        console.log("There's hidable elements: disappearElements.length > 0");
         disappear(disappearElements);
     }
     /*resize*/
     var resizeElements = $('[reduce]');
     if (resizeElements.length > 0) {
-        console.log("there's reduceable elements: resizeElements.length > 0");
+        console.log("There's reduceable elements: resizeElements.length > 0");
         reduce(resizeElements);
     }
-    
 }
 
 function disappear(items) {
     for(var i=0;i<items.length;i++){
-        console.log(items[i]); 
-        //console.log(items[i].getAttribute('disappear')); 
+        console.log(items[i]);
+        //console.log(items[i].getAttribute('disappear'));
         var device = items[i].getAttribute('disappear');
         disappearHandler(items[i],device);
     }
@@ -58,7 +62,7 @@ function disappearHandler(item, device){
     getCurrentSize();
     console.log(currentWidth);
     console.log();
-   
+
     if(currentWidth <= window[device]){
         console.log(device);
         item.style.display = "none";
@@ -76,7 +80,7 @@ function reduce(items) {
 
 function reduceHandler(item,attribute){
     getCurrentSize();
-    console.log(window[attribute[0]]);   
+    console.log(window[attribute[0]]);
     if(currentWidth <= window[attribute[0]]){
         item.style.width = attribute[1];
     }else{
@@ -84,17 +88,82 @@ function reduceHandler(item,attribute){
     }
 }
 
-function handleOrientation(event) {
-  var absolute = event.absolute;
-  var alpha    = event.alpha;
-  var beta     = event.beta;
-  var gamma    = event.gamma;
-    
-    document.getElementById("absolute").innerHTML="<h3>absolute: "+absolute+"</h3>";
-    document.getElementById("alpha").innerHTML="<h3>alpha: "+alpha+"</h3>";
-    document.getElementById("beta").innerHTML="<h3>beta: "+beta+"</h3>";
-    document.getElementById("gamma").innerHTML="<h3>gamma: "+gamma+"</h3>";
-
-
+function toggleExecution(){
+  var toggleElements = $('[toggle]');
+  if (toggleElements.length > 0) {
+    console.log("There's toggleable elements: ToggleElements.length > 0")
+    toggle(toggleElements);
+  }
 }
 
+
+/*function toggle(items){
+    var button
+    for (var i=0;i<items.length;i++){
+      button = document.createElement('button');
+      button.innerHTML = 'BUTTON BUTTON BUTTON';
+      button.className += 'responsivejsbutton';
+      items[i].insertAdjacentElement('beforebegin',button);
+      items[i].style.transition  = "0.5s";
+      items[i].style.display  = "none";
+    }
+    toggleHandler(items);
+}
+
+function toggleHandler(hiddenitem){
+  var timesclicked = 0;
+  var button = document.getElementsByClassName("responsivejsbutton");
+    for(var i = 0;i<hiddenitem.length;i++){
+      if(button[i].nextSibling == hiddenitem[i]){
+        console.log()
+        button[i].addEventListener("click",function(){finishHandling(i)});
+      }
+    }
+    function finishHandling(index){
+      timesclicked++;
+      console.log("CLICKS: "+timesclicked + "index: "+index);
+      if (timesclicked % 2==0 && timesclicked>0){
+        console.log(button[i]);
+        hiddenitem[].classList.add("hidden");
+      }else{
+        hiddenitem[0].classList.remove("hidden");
+      }
+      
+
+    }
+}
+*/
+
+function toggle(items){
+  items.forEach(function(items){
+      button = document.createElement('button');
+      button.innerHTML = 'BUTTON BUTTON BUTTON';
+      button.className += 'responsivejsbutton';
+      items.insertAdjacentElement('beforebegin',button);
+      items.style.transition  = "0.5s";
+      items.classList.add("hidden");
+      var nextItem = button.nextSibling;
+      button.addEventListener("click",function(){toggleHandler(nextItem)});
+  });
+    /*for (var i=0;i<items.length;i++){
+      button = document.createElement('button');
+      button.innerHTML = 'BUTTON BUTTON BUTTON';
+      button.className += 'responsivejsbutton';
+      items[i].insertAdjacentElement('beforebegin',button);
+      items[i].style.transition  = "0.5s";
+      items[i].style.display  = "none";
+    }*/
+   
+}
+
+function toggleHandler(hiddenitem){
+  console.log(hiddenitem);
+  if(hiddenitem.classList.contains("hidden")){
+    hiddenitem.classList.remove("hidden");
+  }else{
+    hiddenitem.classList.add("hidden");
+  }
+}
+
+
+//*ADD MODALS!!!*//
